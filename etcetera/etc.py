65b11d5e2a283f8eb18014b6120131639ff66058
Faisal <md.faisal1899@yahoo.com>
2022-09-12 16:56:32 +0600

    oop- Class fundamental

--- etcetera/etc.py
+++ etcetera/etc.py
@@ -2,25 +2,12 @@ import os
 from ast import arguments
 from glob import glob
 
-os.system('cls')
-print('\n\n') 
+import cowsay
+import pyttsx3
 
-students = [
-{"name": "Hermione", "house": "Gryffindor"},
-{"name": "Harry", "house": "Gryffindor"},
-{"name": "Ron", "house": "Gryffindor"},
-{"name": "Draco", "house": "Slytherin"},
-]
+engine = pyttsx3.init()
 
-gryffindors = [student["name"] for student in students if student["house"] == "Gryffindor"]
-
-print(sorted(gryffindors))
-
-
-def is_gryffindor(s):
-    return s["house"] == "Gryffindor"
-
-gryffindors = filter(is_gryffindor, students)
-
-for gryffindor in sorted(gryffindors, key = lambda s: s["name"]):
-    print(gryffindor["name"])
+this = input("What's this? ")
+cowsay.turtle(this)
+engine.say(this)
+engine.runAndWait()
--- oop/student.py
+++ oop/student.py
@@ -6,25 +6,45 @@ print('\n\n')
 
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Name is Missing.")
-        if house not in ["Gryffindor", "Hufflepuf", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid House.")
         self.name = name
         self.house = house
-
+    
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Provide a name.")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Ravenclaw", "Slytherin"]:
+            raise ValueError("House is not valid.")
+        self._house = house
+
+
+
+
 def main():
     student = get_student()
+    student.house = "Slytherin"
     print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
     return Student(name, house)
-    
+
 
 if __name__ == "__main__":
     main()

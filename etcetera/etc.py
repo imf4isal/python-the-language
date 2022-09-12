cf90866a5985f2bc17cd5d3dcecf80875940e3be
Faisal <md.faisal1899@yahoo.com>
2022-09-12 09:26:11 +0600

    global variable

---
+++ etcetera/etc.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+
+os.system('cls')
+print('\n\n') 
+
+
+class Account:
+    def __init__(self):
+        self._balance = 0
+    
+    @property
+    def balance(self):
+        return self._balance
+    
+    def deposit(self, n):
+        self._balance += n
+    
+    def withdraw(self, n):
+        self._balance -= n
+
+
+def main():
+    account = Account()
+    print("Balance:", account.balance)
+    account.deposit(100)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance:", account.balance)
+
+
+
+
+if __name__ == "__main__":
+    main()

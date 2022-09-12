58f367c0746d1dde13380bad94a8563df08c98b3
Faisal <md.faisal1899@yahoo.com>
2022-09-12 11:10:45 +0600

    args, kwargs

--- etcetera/etc.py
+++ etcetera/etc.py
@@ -5,23 +5,9 @@ from glob import glob
 os.system('cls')
 print('\n\n') 
 
-# import sys
 
-# if len(sys.argv) == 1:
-#     print("meow")
-# elif len(sys.argv) == 3:
-#     n = int(sys.argv[2])
-#     for _ in range(n):
-#         print("meow")
-# else:
-#     print("usage: meows.py [-n NUMBER]")
+def f(*arg, **kwargs):
+    print("positional:", arg)
+    print("named:", kwargs)
 
-
-import argparse
-
-parser = argparse.ArgumentParser(description="Meow like a cat.")
-parser.add_argument("-n",default= 1, help= "number of meow", type=int)
-args = parser.parse_args()
-
-for _ in range(int(args.n)):
-    print("meow")
+f(100, 50, 25, name = "Faisal")
